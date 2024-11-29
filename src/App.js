996d36f21eed11c479e1588f82016db5cc172c03
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-29 15:39:17 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ContactList from "./Components/ContactList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ContactList />
+    </div>
+  );
+}
